14b87c17cf96f7a3aeb728bf556929e9cb0dfcda
Aleksander Vae Haaland <av.haaland@stud.uis.no>
2026-03-31 01:01:31 +0200

    supabase connection and project now running

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -5,7 +5,7 @@ export function createClient() {
   const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
     {
       cookies: {
         getAll() {
